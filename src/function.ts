1ed22e9ec2f4dc3a7a4f304f4dc559f989ceb1d9
Saif Maamun <sadifmamun@gmail.com>
2024-06-22 09:44:19 +0600

    function

--- src/function.ts
+++ src/function.ts
@@ -7,3 +7,17 @@ function add(n1: number, n2: number): number {
 const addArrow = (n1: number, n2: number): number => {
   return n1 + n2;
 };
+
+const student: {
+  nationality: "Bangladesh"; //literal type
+  name: string;
+  marks: number;
+  addMark(mark: number): string;
+} = {
+  nationality: "Bangladesh",
+  name: "mamun",
+  marks: 28,
+  addMark(mark: number): string {
+    return `My mark is${this.marks + mark}`;
+  },
+};
