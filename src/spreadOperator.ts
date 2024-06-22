b7746d1547c94eeb4aa353cc295690542cbfe68c
Saif Maamun <sadifmamun@gmail.com>
2024-06-22 17:02:17 +0600

    spread operator

---
+++ src/spreadOperator.ts
@@ -0,0 +1,6 @@
+const sscTeacher: string[] = ["raju", "pavel", "niloy"];
+const hscTeacher: string[] = ["chandan", "babu", "mithu"];
+
+const teachers = [];
+teachers.push(...sscTeacher, ...hscTeacher);
+console.log(teachers);
