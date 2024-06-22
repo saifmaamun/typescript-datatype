8daf2698558cc83f13e88563c3eb3ea54b79ae68
Saif Maamun <sadifmamun@gmail.com>
2024-06-22 09:37:05 +0600

    obj

---
+++ src/function.ts
@@ -0,0 +1,9 @@
+// normal function
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+// arrow function
+const addArrow = (n1: number, n2: number): number => {
+  return n1 + n2;
+};
